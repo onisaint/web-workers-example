7a4b0d99d7c3b15c63af8e7bfd8bc33f5425c900
Madhu Kumar D S <madhudskumar@live.in>
2018-03-07 23:16:09 +0530

    +:webworkers fib sync and async

---
+++ worker.js
@@ -0,0 +1,49 @@
+const CALC_FIB_SYNC = 'calculateFibbonaciSync',
+    CALC_FIB_ASYNC = 'calculateFibbonaciAsync';
+
+let isCalled = false;
+
+self.addEventListener('message', ({ data }) => {
+    switch (data) {
+        case CALC_FIB_SYNC: {
+            const result = calculateFibbonaciSequence(100 ** 2);
+            self.postMessage({
+                result,
+                type: CALC_FIB_SYNC
+            });
+            break;
+        }
+        case CALC_FIB_ASYNC: {
+            const result = calculateFibbonaciGenerator().next().value;
+            self.postMessage({
+                result,
+                type: CALC_FIB_ASYNC
+            });
+            break;
+        }
+        default: break;
+    }
+})
+
+// workers
+function* calculateFibbonaciGenerator() {
+    let [a, b] = [0, 1];
+
+    while (true) {
+        yield a;
+        [a, b] = [b, a + b];
+    }
+}
+
+function calculateFibbonaciSequence(n) {
+    const fibSeries = [];
+    let [a, b] = [0, 1];
+
+    let i = 0;
+    while (i++ != n) {
+        fibSeries.push(a);
+        [a, b] = [b, a + b];
+    }
+
+    return fibSeries;
+}
